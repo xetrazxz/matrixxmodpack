574fab5c245124e28d0bfb75c433ddad7f1aa4bc
xetrazxz <xetrazx@gmail.com>
2025-06-12 21:07:33 +0530

    Last Sync: 2025-06-12 21:07 (Mobile)

---
+++ module/services.sh
@@ -0,0 +1,9 @@
+#
+setprop ro.matrixx.chipset Snapdragon® 4 Gen 2
+setprop ro.matrixx.display_resolution FHD
+setprop ro.matrixx.display.version Matrixx-v99.9.0-Mod
+setprop ro.matrixx.maintainer xetrazxz
+setprop ro.matrixx.release.type Unofficial
+setprop ro.sf.blurs_are_expensive 0
+setprop ro.surface_flinger.supports_background_blur 0
+#
